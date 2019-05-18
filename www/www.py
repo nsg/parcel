fb52437ada37c983e9c17b66de41b4c62ac6caf3
Stefan Berggren <nsg@nsg.cc>
2019-05-18 19:09:12 +0200

    API: get/set config, list Ansible output.
    * Output Ansible in JSON
    * Fix www and stats backens

--- www/www.py
+++ www/www.py
@@ -1,7 +1,11 @@
 import os
+import subprocess
+import pathlib
 import responder
+import json
 
 ROOT = "{}/wwwroot".format(os.environ['SNAP'])
+DATA = os.environ['SNAP_DATA']
 
 api = responder.API(
     static_dir="{}/static".format(ROOT),
@@ -10,9 +14,72 @@ api = responder.API(
 
 api.add_route("/static/", static=True)
 
+def get_config(val):
+    stdout, stderr = subprocess.Popen(["snapctl", "get", val],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT).communicate()
+    return stdout.decode('utf-8').strip()
+
+def set_config(key, val):
+    stdout, stderr = subprocess.Popen(["snapctl", "set", "{}=\"{}\"".format(key, val)],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT).communicate()
+    if stderr:
+        return False
+    return True
+
 @api.route("/")
 async def index(req, resp):
     resp.html = api.template('index.html')
+
+@api.route("/api/config")
+async def config(req, resp):
+
+    if req.method == "get":
+        values = [
+            "domains",
+            "dkim-domains",
+            "postfix-domain",
+            "postfix-origin",
+            "postfix-relay-host",
+            "use-snakeoil-cert",
+            "haproxy-username",
+            "haproxy-password"
+        ]
+        config = {}
+        for val in values:
+            config[val] = get_config(val)
+        resp.media = { "config": config }
+    elif req.method == "post":
+
+        data = await req.media()
+        log = []
+        for upd in data['update']:
+            log.append("Set {} to {}".format(upd['key'], upd['value']))
+            set_config(upd['key'], upd['value'])
+
+        resp.media = {
+            "success": True,
+            "log": log
+        }
+    else:
+        resp.media = {"success": False}
+
+@api.route("/api/ansible")
+async def ansible(req, resp):
+    if req.method == "get":
+        with open("{}/provision.log".format(DATA)) as f:
+            data = f.read()
+        try:
+            json_data = json.loads(data)
+        except:
+            json_data = {}
+        resp.media = {"data": json_data}
+    elif req.method == "post":
+        pathlib.Path("{}/do-provision".format(DATA)).touch(exist_ok=True)
+        resp.media = {"success": True}
+    else:
+        resp.media = {"success": False}
 
 def main():
     api.run()
