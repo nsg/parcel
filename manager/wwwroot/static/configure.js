3489c9861c289f0d9016acc1eb77e1177da97146
Stefan Berggren <nsg@nsg.cc>
2019-06-06 18:47:40 +0200

    Move jquery to head.

--- manager/wwwroot/static/configure.js
+++ manager/wwwroot/static/configure.js
@@ -8,3 +8,12 @@ function get_config() {
         console.log(ret);
     });
 }
+
+function save_lxd_config() {
+    var remote = document.getElementById("lxd-remote");
+    var password = document.getElementById("lxd-password");
+
+    console.log(submit.parentNode);
+
+    return false;
+}
