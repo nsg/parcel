82537de8de1845bee585286d59778857f9b70ef4
Stefan Berggren <nsg@nsg.cc>
2019-08-10 17:00:56 +0200

    Add simple validations for configuration page.

--- manager/wwwroot/static/configure.js
+++ manager/wwwroot/static/configure.js
@@ -3,10 +3,47 @@ window.onload = function() {
     fix_code_blocks()
 };  
 
+function append_err(obj, err) {
+    let p = document.createElement("p");
+    p.innerText = err;
+    obj.style.display = "block";
+    obj.appendChild(p);
+}
+
 function save_data(save_button, values_str) {
+    let orig_save_button_string = "Validate & Save";
     save_button.classList.add("button-saving");
     save_button.innerHTML = "Saving ..."
     var values = values_str.split(",");
+    var abort = false;
+
+    values.forEach(element => {
+        let msg = validate(element, document.getElementById(element).value);
+        if (msg[0]) {
+            document.getElementById(element).classList.remove("is-invalid");
+            document.getElementById(element).classList.add("is-valid");
+        } else {
+            document.getElementById(element).classList.remove("is-valid");
+            document.getElementById(element).classList.add("is-invalid");
+            save_button.classList.remove("button-saving");
+            save_button.classList.remove("btn-primary");
+            save_button.classList.add("btn-danger");
+            save_button.innerHTML = "Invalid configuration, not saved!"
+            let errmsg = save_button.parentNode.parentNode.getElementsByClassName("form-error-message")
+            append_err(errmsg[0], msg[1])
+            abort = true;
+        }
+    });
+
+    if (abort) {
+        return false;
+    }
+
+    save_button.classList.remove("btn-danger");
+    save_button.classList.add("btn-primary");
+    let err_div = save_button.parentNode.parentNode.getElementsByClassName("form-error-message")[0]
+    err_div.style.display = "none";
+    err_div.innerHTML = "";
 
     var update = []
     values.forEach(element => {
@@ -25,11 +62,27 @@ function save_data(save_button, values_str) {
     }).done(function() {
         setTimeout(function() {
             save_button.classList.remove("button-saving");
-            save_button.innerHTML = "Save";
+            save_button.innerHTML = orig_save_button_string;
         }, 1000);
     });
 
     return false;
+}
+
+function validate(field, data) {
+    let msg = [];
+
+    switch(field) {
+        case "origin":
+            if (data.match(/[, ]/)) msg.push("Origin must be a valid single domain/FQDN.");
+            break;
+        case "domains":
+            if (data.match(/[ ]/)) msg.push("Spaces are not allowed in domains.")
+            if (data.match(/[,]$/)) msg.push("Remove last \",\" from domains.")
+            break;
+    }
+
+    return [msg.length == 0, msg.join("; ")];
 }
 
 function update_card(values) {
